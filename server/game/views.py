80294da33d5e9733f7a6aee57b1282fc9d36a549
Lewis <lwat0045@uni.sydney.edu.au>
2020-08-29 18:00:09 +1000

    views updated

--- server/game/views.py
+++ server/game/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def is_valid_game_id(id):
+    #TODO check that the id is in the current server database
+    return True
 
 # View to check if user is already in a session
 def index(request):
@@ -11,12 +14,16 @@ def index(request):
     # auto log in:
     if request.sessions.has_key("game_id"):
         game_id = request.sessions["game_id"]
-        #return the game view
-        return render(request, "game.html", {})
-    else:
-        # No session established on this host yet:
 
-        return render(request, "join.html", {})
+        #check id is valid
+        if is_valid_game_id(game_id):
+            #return the game view
+            return render(request, "game.html", {})
+        else:
+            del request.sessions["game_id"]
+    
+    # No session established on this host yet:
+    return render(request, "join.html", {})
 
 
 

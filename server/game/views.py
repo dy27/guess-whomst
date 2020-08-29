2a40dff5e501df38cfaa068c37a55a6cf7f67f1a
Lewis <lwat0045@uni.sydney.edu.au>
2020-08-29 16:29:46 +1000

    Changed

--- server/game/views.py
+++ server/game/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# View to check if user is already in a session
+def index(request):
+
+    game_id = ""
+
+    # auto log in:
+    if request.sessions.has_key("game_id"):
+        game_id = request.sessions["game_id"]
+        #return the game view
+        return render(request, "game.html", {})
+    else:
+        # No session established on this host yet:
+
+        return render(request, "join.html", {})
+
+
+
+# Main game view
+def game(request):
+    return render(request, "game.html", {})
--- server/guesswho/asgi.py
+++ server/guesswho/asgi.py
@@ -14,3 +14,15 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guesswho.settings')
 
 application = get_asgi_application()
+
+
+# Reference: https://dev.to/jaydenwindle/adding-websockets-to-your-django-app-with-no-extra-dependencies-2f6h
+async def application(scope, receive, send):
+    if scope['type'] == 'http':
+        await django_application(scope, receive, send)
+    elif scope['type'] == 'websocket':
+        # Handle websockets
+        #TODO
+        
+    else:
+        raise NotImplementedError(f"Unknown scope type {scope['type']}")
